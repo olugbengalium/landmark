acda7a8bedbf54858d478c953fce7f3edf7680bb
Akinbande Gbenga <olugbengalium@gmail.com>
2023-02-18 20:08:46 +0100

    Initial Commit

---
+++ landmark/landmarkApp.swift
@@ -0,0 +1,17 @@
+//
+//  landmarkApp.swift
+//  landmark
+//
+//  Created by Gbenga Abayomi on 18/02/2023.
+//
+
+import SwiftUI
+
+@main
+struct landmarkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
